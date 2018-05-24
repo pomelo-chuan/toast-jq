7f06728447934e1119d6f5487aa0d13b88353bd2
chenchuan <chuan1992@hotmail.com>
2018-05-24 15:56:41 +0800

    :art:

--- src/pomelo-toast.js
+++ src/pomelo-toast.js
@@ -1,67 +1,93 @@
-const pomeloToast = function(content) {
+class PomeloToast {
+    constructor() {
+        this.COLOR = {
+            white: 'black',
+            primary: 'white',
+            error: 'white',
+            success: 'white',
+            warning: 'white',
+            blue: 'white',
+        };
+        this.BG_COLOR = {
+            white: 'white',
+            primary: '#757575',
+            error: '#FF3D00',
+            success: '#1DE9B6',
+            warning: '#FFC400',
+            blue: '#1E88E5',
+        };
+    }
 
-  let type = 'primary';
-  let time = 3000;
-  let text = '';
-  const id = 'pomelo-toast-id-' + new Date().getTime();
-  
-  if (typeof content === 'string' || typeof content === 'number') {
-    type = 'primary';
-    text = content;
-  } else {
-    if (content.type) {
-      type = content.type;
+    checkEnvironment() {
+        if (!window.$) {
+            throw new Error('Please include Jquery or Zepto before!');
+        }
     }
-    if (content.time) {
-      time = content.time;
-    }
-    if (content.text) {
-      text = content.text;
-    }
-  }
-  if ($('#pomelo-toast-container').length === 0) {
-    $('body').append(`
-      <div id="pomelo-toast-container">
-      </div>
-      `);
-  }
-  pomeloToastAppend({
-    id: id,
-    text: text,
-    type: type,
-  });
-  pomeloToastRemove({
-    id: id,
-    time: time,
-  })
-};
 
-// append DOM
-function pomeloToastAppend(content) {
-  const {
-    id,
-    text,
-    type,
-  } = content;
-  $('#pomelo-toast-container').append(`
-        <div class="pomelo-toast-${type}" id="${id}">${text}</div>
-    `);
-}
-
-// reomove DOM
-function pomeloToastRemove(content) {
-  const {
-    id,
-    time,
-  } = content;
-  setTimeout(() => {
-    $(`#${id}`).addClass('pomelo-toast-down');
-  }, time - 800);
-  setTimeout(() => {
-    if ($('#pomelo-toast-container')[0]['children'].length < 2) {
-      $('#pomelo-toast-container').remove();
-      return;
+    append(content) {
+        const {
+            id,
+            text,
+            type,
+        } = content;
+        $('#pomelo-toast-container').append(`<div style="${this.color(type)}" id="${id}">${text}</div>`);
     }
-    $(`#${id}`).remove();
-  }, time);
+
+    remove(content) {
+        const {
+            id,
+            time,
+        } = content;
+        setTimeout(() => {
+            $(`#${id}`).addClass('pomelo-toast-down');
+        }, time - 800);
+        setTimeout(() => {
+            if ($('#pomelo-toast-container')[0]['children'].length < 2) {
+                $('#pomelo-toast-container').remove();
+                return;
+            }
+            $(`#${id}`).remove();
+        }, time);
+    }
+
+    color(type) {
+        return 'color: :color; background-color: :background-color;'
+            .replace(':color', this.COLOR[type] ? this.COLOR[type] : 'white')
+            .replace(':background-color', this.BG_COLOR[type] ? this.BG_COLOR[type] : '#757575');
+    }
+
+    show(content) {
+        this.checkEnvironment();
+        let type = 'primary';
+        let time = 3000;
+        let text = '';
+        const id = 'pomelo-toast-id-' + new Date().getTime();
+
+        if (typeof content === 'string' || typeof content === 'number') {
+            type = 'primary';
+            text = content;
+        } else {
+            if (content.type) {
+                type = content.type;
+            }
+            if (content.time) {
+                time = content.time > 2000 ? content.time : 2000;
+            }
+            if (content.text) {
+                text = content.text;
+            }
+        }
+        if ($('#pomelo-toast-container').length === 0) {
+            $('body').append('<div id="pomelo-toast-container"></div>');
+        }
+        this.append({
+            id: id,
+            text: text,
+            type: type,
+        });
+        this.remove({
+            id: id,
+            time: time,
+        })
+    }
 }
